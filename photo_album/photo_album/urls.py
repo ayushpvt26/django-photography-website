6bceba57cc0877ddcc4c624037d22834794a9469
Daniel Davies <daniel.davies@aviva.co.uk>
2012-10-15 13:13:40 +0100

    About page

--- photo_album/photo_album/urls.py
+++ photo_album/photo_album/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,6 +10,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'photo_album.views.home', name='home'),
     url(r'^gallery/', include('album.urls')),
+
+    url(r'^about/', TemplateView.as_view(template_name="about.html"), name="about-page"),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
