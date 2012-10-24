433ac69cd503c963e1ba48ef86833df3b69973ba
Daniel Davies <daniel.davies@aviva.co.uk>
2012-10-24 14:50:06 +0100

    Tidy up

--- photo_album/settings_local.py
+++
@@ -1,9 +0,0 @@
-from photo_album.settings import *
-
-MAP_API_KEY = 'AIzaSyADKWJx1XBGYuf4BnL67nnZ4K9eZxu_Zrs'
-
-DISQUS_API_KEY = 't71Ww5ptMC6Qcno6tANPiwFKtLXiL3spDDfvesKDTeQp8ZPGmVaH5Fckl6l4hlmb'
-
-DISQUS_WEBSITE_SHORTNAME = 'danieldaviesphotography'
-
-TEMPLATE_DEBUG = True
